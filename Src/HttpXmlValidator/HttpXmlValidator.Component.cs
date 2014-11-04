9084f824a29cf575597323e5736f8e8e7b734fe3
robinhultman <robin.hultman@gmail.com>
2014-11-04 15:35:05 +0100

    Created HttpXmlValidator project New pipeline component that should validate an XML message and return an HTTP 400 if the XML is not valid.

---
+++ Src/HttpXmlValidator/HttpXmlValidator.Component.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace BizTalkComponents.PipelineComponents.HttpXmlValidator
+{
+    public partial class HttpXmlValidator
+    {
+        public string Name { get { return "HttpXmlValidator"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description { get
+        {
+            return
+                "Validates incomming XML messages and route invalid messages back to the client with HTTP 400 bad request.";
+        } }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return null;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+
+        public void GetClassID(out Guid classID)
+        {
+            classID = new Guid("310651A9-4963-4F41-A82F-30B2F1A9613D");
+        }
+
+        public void InitNew()
+        {
+            
+        }
+    }
+}
---
+++ Src/HttpXmlValidator/HttpXmlValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+
+namespace BizTalkComponents.PipelineComponents.HttpXmlValidator
+{
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("13975C44-8A9D-4AA8-8443-62ACA160C0FC")]
+    [ComponentCategory(CategoryTypes.CATID_Validate)]
+    public partial class HttpXmlValidator : IComponent, IBaseComponent, IComponentUI, IPersistPropertyBag
+    {
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
